afab2d010bd439c91e7202175116f7e1eb779b06
micha <micha@MICHAEL-PC>
2024-01-22 06:24:51 +0200

    a

--- Context/obj/Debug/net8.0/Context.AssemblyInfo.cs
+++ Context/obj/Debug/net8.0/Context.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Context")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f98fe432659bbdc110fa531e25370d2483d9e0f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Context")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Context")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
